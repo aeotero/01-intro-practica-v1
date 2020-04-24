29097ec50c1181231a9ea272f2f3461228c5df87
agustin otero <popal9193@gmail.com>
2020-04-23 21:20:03 -0300

    comentado lo que hay que hacer en el 1.5

---
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio15.java
@@ -0,0 +1,13 @@
+package ar.edu.unahur.obj2.ejercicio1;
+
+public class Ejercicio15 {
+    public static void main(String[] args){
+        //Son 3 array ,1 para el texto , otro para el alfabeto , y el tercero para el resultado.
+        //Despues un for que recorra caracteres 1 por 1  del texto y valla preguntado en un for anidado
+        // con el alfabeto y sumando en el tercer array una unidad numerica por posicion correspondiente
+        //a la letra.
+        // al final recorrer el tercer array mostrando los valores por letras.
+    }
+
+
+}
